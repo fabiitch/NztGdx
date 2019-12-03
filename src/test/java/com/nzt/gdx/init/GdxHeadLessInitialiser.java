b48dad111c21236e98f3f39d9c35439bd020f66e
fabiitch <foccelli@reservit.com>
2019-12-03 11:53:55 +0100

    remove log in unit test

--- src/test/java/com/nzt/gdx/init/GdxHeadLessInitialiser.java
+++ src/test/java/com/nzt/gdx/init/GdxHeadLessInitialiser.java
@@ -1,13 +1,20 @@
 package com.nzt.gdx.init;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 
 public abstract class GdxHeadLessInitialiser {
 	public GdxHeadLessInitialiser() {
-		System.out.println("Gdx HeadlessFake init");
+
 		HeadlessApplicationConfiguration conf = new HeadlessApplicationConfiguration();
 		HeadlessApplication application = new HeadlessApplication(new FakeMain(), conf);
+		application.setLogLevel(Application.LOG_NONE);
+		application.log("Unit Test initialisation", "HeadlessFake");
 	}
 
+	protected void enableLog() {
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+	}
 }
