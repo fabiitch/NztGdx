d33f08b8dc5e5ff1dc0fc4fef197070c8adf7d1f
fabiitch <fabien.occelli@hotmail.fr>
2019-12-12 21:03:26 +0100

    NZ utils class

---
+++ src/main/java/com/nzt/gdx/utils/NZ.java
@@ -0,0 +1,32 @@
+package com.nzt.gdx.utils;
+
+public class NZ {
+
+	public static boolean isNull(Object object) {
+		return object == null;
+	}
+
+	public static boolean isNull(Object... objects) {
+		final int n = objects.length;
+		for (int i = 0; i < n; i++) {
+			if (objects[i] != null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isNotNull(Object object) {
+		return object != null;
+	}
+
+	public static boolean isNotNull(Object... objects) {
+		final int n = objects.length;
+		for (int i = 0; i < n; i++) {
+			if (objects[i] == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/test/java/com/nzt/gdx/utils/NzTest.java
@@ -0,0 +1,33 @@
+package com.nzt.gdx.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class NzTest {
+	@Test
+	public void isNullTest() {
+
+		Object nullObject = null;
+		Object notNullObject = new Object();
+
+		assertTrue(NZ.isNull(nullObject));
+		assertFalse(NZ.isNull(notNullObject));
+
+		assertTrue(NZ.isNull(nullObject, nullObject));
+		assertFalse(NZ.isNull(notNullObject, nullObject));
+	}
+	@Test
+	public void isNotNullTest() {
+
+		Object nullObject = null;
+		Object notNullObject = new Object();
+
+		assertFalse(NZ.isNotNull(nullObject));
+		assertTrue(NZ.isNotNull(notNullObject));
+
+		assertFalse(NZ.isNotNull(nullObject, nullObject));
+		assertTrue(NZ.isNotNull(notNullObject, notNullObject));
+	}
+}
