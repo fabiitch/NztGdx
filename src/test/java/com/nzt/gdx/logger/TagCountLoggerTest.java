da321b0de3196844fb53cda16fcc2cc8dbb38927
fabiitch <fabien.occelli@hotmail.fr>
2019-12-14 19:50:20 +0100

    update tu for taglogger

--- src/test/java/com/nzt/gdx/logger/TagCountLoggerTest.java
+++ src/test/java/com/nzt/gdx/logger/TagCountLoggerTest.java
@@ -3,6 +3,9 @@ package com.nzt.gdx.logger;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,9 +18,16 @@ public class TagCountLoggerTest extends GdxHeadLessInitialiser {
 		PERFORMANCE, INPUT
 	}
 
+	private static Map<Enum<?>, LogCountTagValues> tagCountMap;
+
+	@SuppressWarnings("unchecked")
 	@Before
-	public void initTagLogger() {
-		TagCountLogger.clear();
+	public void initTagLogger()
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		TagCountLogger.clearTags();
+		Field fieldTagCountMap = TagCountLogger.class.getDeclaredField("tagCountMap");
+		fieldTagCountMap.setAccessible(true);
+		tagCountMap = (Map<Enum<?>, LogCountTagValues>) fieldTagCountMap.get(null);
 	}
 
 	@Test
@@ -26,10 +36,10 @@ public class TagCountLoggerTest extends GdxHeadLessInitialiser {
 		TagCountLogger.log(LogTagEnum.PERFORMANCE, "log 2");
 		TagCountLogger.log(LogTagEnum.PERFORMANCE, "log 3");
 		// test map of TagLogger and TagCounter logger
-		assertEquals(TagCountLogger.tagCountMap.size(), 1);
-		assertEquals(TagLogger.tagMap.size(), 1);
+		assertEquals(tagCountMap.size(), 1);
+		assertEquals(TagLogger.getTags().size(), 1);
 
-		LogCountTagValues logTagValues = TagCountLogger.tagCountMap.get(LogTagEnum.PERFORMANCE);
+		LogCountTagValues logTagValues = tagCountMap.get(LogTagEnum.PERFORMANCE);
 		assertTrue(logTagValues != null);
 		assertEquals(logTagValues.count, 3);
 	}
@@ -40,7 +50,7 @@ public class TagCountLoggerTest extends GdxHeadLessInitialiser {
 		TagCountLogger.log(LogTagEnum.PERFORMANCE, "log 2");
 		TagCountLogger.log(LogTagEnum.PERFORMANCE, "log 3");
 		// test map of TagLogger and TagCounter logger
-		LogCountTagValues logTagValues = TagCountLogger.tagCountMap.get(LogTagEnum.PERFORMANCE);
+		LogCountTagValues logTagValues = tagCountMap.get(LogTagEnum.PERFORMANCE);
 		assertTrue(logTagValues != null);
 		assertEquals(logTagValues.count, 3);
 		TagCountLogger.resetTag(LogTagEnum.PERFORMANCE);
--- src/test/java/com/nzt/gdx/logger/TagLoggerTest.java
+++ src/test/java/com/nzt/gdx/logger/TagLoggerTest.java
@@ -4,29 +4,39 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.lang.reflect.Field;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class TagLoggerTest {
 
+	private static Map<Enum<?>, Boolean> tagMap;
+
+	@SuppressWarnings("unchecked")
 	@Before
-	public void initTagLogger() {
-		TagLogger.clear();
+	public void initTagLogger()
+			throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		TagLogger.clearTags();
+		Field fieldTagMap = TagLogger.class.getDeclaredField("tagMap");
+		fieldTagMap.setAccessible(true);
+		tagMap = (Map<Enum<?>, Boolean>) fieldTagMap.get(null);
 	}
 
 	@Test
 	public void activeAndDesactiveTagTest() {
 		TagLogger.activeTag(LogTagBase.PERFORMANCE);
-		assertEquals(TagLogger.tagMap.size(), 1);
-		Boolean tagActive = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
+		assertEquals(TagLogger.getTags().size(), 1);
+		Boolean tagActive = tagMap.get(LogTagBase.PERFORMANCE);
 		assertTrue(tagActive);
 
 		TagLogger.desactiveTag(LogTagBase.PERFORMANCE);
-		tagActive = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
+		tagActive = tagMap.get(LogTagBase.PERFORMANCE);
 		assertFalse(tagActive);
 
 		TagLogger.activeTag(LogTagBase.PERFORMANCE);
-		tagActive = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
+		tagActive = tagMap.get(LogTagBase.PERFORMANCE);
 		assertTrue(tagActive);
 	}
 
@@ -34,21 +44,21 @@ public class TagLoggerTest {
 	public void desactiveAndActiveAllTagsTest() {
 		TagLogger.activeTag(LogTagBase.PERFORMANCE);
 		TagLogger.activeTag(LogTagBase.INPUT);
-		assertEquals(TagLogger.tagMap.size(), 2);
-		Boolean tag1Active = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
-		Boolean tag2Active = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
+		assertEquals(TagLogger.getTags().size(), 2);
+		Boolean tag1Active = tagMap.get(LogTagBase.PERFORMANCE);
+		Boolean tag2Active = tagMap.get(LogTagBase.PERFORMANCE);
 		assertTrue(tag1Active);
 		assertTrue(tag2Active);
 
 		TagLogger.desactiveAllTag(LogTagBase.class);
-		tag1Active = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
-		tag2Active = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
+		tag1Active = tagMap.get(LogTagBase.PERFORMANCE);
+		tag2Active = tagMap.get(LogTagBase.PERFORMANCE);
 		assertFalse(tag1Active);
 		assertFalse(tag2Active);
 
 		TagLogger.activeAllTag(LogTagBase.class);
-		tag1Active = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
-		tag2Active = TagLogger.tagMap.get(LogTagBase.PERFORMANCE);
+		tag1Active = tagMap.get(LogTagBase.PERFORMANCE);
+		tag2Active = tagMap.get(LogTagBase.PERFORMANCE);
 		assertTrue(tag1Active);
 		assertTrue(tag2Active);
 	}
