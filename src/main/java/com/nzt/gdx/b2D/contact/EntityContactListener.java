403fc30b8f9cf49ba1f316f99ca2cb7190f50b6c
Fabiitch <fabien.occelli@hotmail.fr>
2020-09-16 16:06:00 +0200

    remove contact listener for ashley

--- src/main/java/com/nzt/gdx/b2D/contact/EntityContactListener.java
+++ src/main/java/com/nzt/gdx/b2D/contact/EntityContactListener.java
@@ -1,37 +1,39 @@
 package com.nzt.gdx.b2D.contact;
 
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
-import com.nzt.gdx.ashley.entities.EntityWrapper;
+import com.nzt.gdx.ashley.components.TypeComponent;
 import com.nzt.gdx.logger.tag.LogTagBase;
 import com.nzt.gdx.logger.tag.TagLogger;
 
-public abstract class EntityContactListener<E extends EntityWrapper> implements ContactListener {
+public abstract class EntityContactListener implements ContactListener {
 
-	@SuppressWarnings("unchecked")
-	@Override
+	private ComponentMapper<TypeComponent> typeMapper = ComponentMapper.getFor(TypeComponent.class);
+
 	public void beginContact(Contact contact) {
 		Fixture fa = contact.getFixtureA();
 		Fixture fb = contact.getFixtureB();
 
-		E entityA = (E) fa.getBody().getUserData();
-		E entityB = (E) fb.getBody().getUserData();
+		Entity entityA = (Entity) fa.getBody().getUserData();
+		Entity entityB = (Entity) fb.getBody().getUserData();
 		debugEvent("Begin Contact", entityA, entityB);
-		
+
 		doBeginContact(entityA, entityB);
 	}
 
-	public abstract void doBeginContact(E entityA, E entityB);
+	public abstract void doBeginContact(Entity entityA, Entity entityB);
 
 	@Override
 	public void endContact(Contact contact) {
 		Fixture fa = contact.getFixtureA();
 		Fixture fb = contact.getFixtureB();
-		EntityWrapper entityA = (EntityWrapper) fa.getBody().getUserData();
-		EntityWrapper entityB = (EntityWrapper) fb.getBody().getUserData();
+		Entity entityA = (Entity) fa.getBody().getUserData();
+		Entity entityB = (Entity) fb.getBody().getUserData();
 		debugEvent("End Contact", entityA, entityB);
 	}
 
@@ -47,8 +49,9 @@ public abstract class EntityContactListener<E extends EntityWrapper> implements 
 
 	}
 
-	private void debugEvent(String eventName, EntityWrapper entityA, EntityWrapper entityB) {
-		TagLogger.log(LogTagBase.B2D_CONTACT, eventName, entityA.name + " / " + entityB.name);
+	private void debugEvent(String eventName, Entity entityA, Entity entityB) {
+		TagLogger.log(LogTagBase.B2D_CONTACT, eventName,
+				typeMapper.get(entityA).name + " / " + typeMapper.get(entityB).name);
 	}
 
 	/**
@@ -60,19 +63,19 @@ public abstract class EntityContactListener<E extends EntityWrapper> implements 
 	 * @param entityB
 	 * @return
 	 */
-	protected boolean testContact(Class<? extends E> class1, Class<? extends E> class2, E entityA, E entityB) {
-		if (entityA.getClass() == class1 && entityB.getClass() == class2
-				|| entityB.getClass() == class1 && entityA.getClass() == class2) {
+	protected boolean testContact(int type1, int type2, Entity entityA, Entity entityB) {
+		if ((typeMapper.get(entityA).mask == type1 && typeMapper.get(entityB).mask == type2)
+				|| (typeMapper.get(entityA).mask == type2 && typeMapper.get(entityB).mask == type1)) {
 			return true;
 		}
 		return false;
 	}
 
-	protected <W extends E> W getGameObject(Class<W> classAsk, E userData1, E userData2) {
-		if (userData1.getClass() == classAsk) {
-			return classAsk.cast(userData1);
+	protected Entity getEntity(int type, Entity userData1, Entity userData2) {
+		if (typeMapper.get(userData1).mask == type) {
+			return userData1;
 		} else {
-			return classAsk.cast(userData2);
+			return userData2;
 		}
 	}
 
