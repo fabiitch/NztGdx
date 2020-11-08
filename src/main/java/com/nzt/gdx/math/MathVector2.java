6f1dcfef528774d2fc3a195472bd8f130f792142
fabiitch <fabien.occelli@hotmail.fr>
2020-11-09 00:49:56 +0100

    woaw

--- src/main/java/com/nzt/gdx/math/MathVector2.java
+++ src/main/java/com/nzt/gdx/math/MathVector2.java
@@ -1,32 +1,35 @@
 package com.nzt.gdx.math;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 
 public class MathVector2 {
 
-	private static Vector2 tmp; // TODO use ? server pb?
+    private static Vector2 tmp; // TODO use ? server pb?
 
-	public static Vector2 getVelocityTo(float time, Vector2 from, Vector2 to) {
-		Vector2 directionTo = directionTo(from, to);
-		float dst = from.dst(to);
-		Vector2 scl = directionTo.scl(dst / time);
-		return scl;
-	}
+    public static Vector2 getVelocityTo(float time, Vector2 from, Vector2 to) {
+        Vector2 directionTo = directionTo(from, to);
+        float dst = from.dst(to);
+        Vector2 scl = directionTo.scl(dst / time);
+        return scl;
+    }
 
-	/**
-	 * direction vector is nor
-	 */
-	public static Vector2 directionTo(Vector2 from, Vector2 to) {
-		return from.cpy().sub(to).nor();
-	}
-	
-	public static double getAngleDegreesOfVector(Vector2 vector) {
-		return Math.atan2(vector.y, vector.x) * 180.0d / Math.PI;
-	}
+    public static Vector directionTo(Vector from, Vector to) {
+        return from.cpy().sub(to).nor();
+    }
 
-	public static double getAngleRadianOfVector(Vector2 vector) {
-		return Math.atan2(vector.y, vector.x);
-	}
-	
+    public static Vector2 directionTo(Vector2 from, Vector2 to) {
+        return from.cpy().sub(to).nor();
+    }
+
+    public static float getAngleDegreesOfVector(Vector2 vector) {
+        return MathUtils.atan2(vector.y, vector.x) * MathUtils.radDeg;
+    }
+
+    public static float getAngleRadianOfVector(Vector2 vector) {
+        return MathUtils.atan2(vector.y, vector.x);
+    }
+
 
 }
--- src/main/java/com/nzt/gdx/math/shape/rectangles/RectangleUtils.java
+++ src/main/java/com/nzt/gdx/math/shape/rectangles/RectangleUtils.java
@@ -5,27 +5,29 @@ import com.badlogic.gdx.math.Rectangle;
 
 public class RectangleUtils {
 
-	public static void centerX(Rectangle rect) {
-		rect.setX(Gdx.graphics.getWidth() / 2 - rect.getWidth() / 2);
-	}
+    public static void centerScreenX(Rectangle rect) {
+        rect.setX(Gdx.graphics.getWidth() / 2 - rect.getWidth() / 2);
+    }
 
-	public static Rectangle allToPPM(Rectangle rect, float PPM) {
-		rect.setX(rect.getX() / PPM);
-		rect.setY(rect.getY() / PPM);
-		rect.setWidth(rect.getWidth() / PPM);
-		rect.setHeight(rect.getHeight() / PPM);
-		return rect;
-	}
+    public static void centerScreenY(Rectangle rect) {
+        rect.setX(Gdx.graphics.getHeight() / 2 - rect.getHeight() / 2);
+    }
 
-	public static Rectangle sizeToPPM(Rectangle rect, float PPM) {
-		rect.setWidth(rect.getWidth() / PPM);
-		rect.setHeight(rect.getHeight() / PPM);
-		return rect;
-	}
+    public static Rectangle allToPPM(Rectangle rect, float PPM) {
+        sizeToPPM(rect, PPM);
+        posToPPM(rect, PPM);
+        return rect;
+    }
 
-	public static Rectangle posToPPM(Rectangle rect, float PPM) {
-		rect.setX(rect.getX() / PPM);
-		rect.setY(rect.getY() / PPM);
-		return rect;
-	}
+    public static Rectangle sizeToPPM(Rectangle rect, float PPM) {
+        rect.setWidth(rect.getWidth() / PPM);
+        rect.setHeight(rect.getHeight() / PPM);
+        return rect;
+    }
+
+    public static Rectangle posToPPM(Rectangle rect, float PPM) {
+        rect.setX(rect.getX() / PPM);
+        rect.setY(rect.getY() / PPM);
+        return rect;
+    }
 }
