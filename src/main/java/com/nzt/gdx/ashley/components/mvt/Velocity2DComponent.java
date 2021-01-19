c06dfdec68a74cd664bc8832b08edc105ca07cb4
Fabiitch <fabien.occelli@hotmail.fr>
2021-01-19 13:18:06 +0100

    add get Dir on velocity compoenent

--- src/main/java/com/nzt/gdx/ashley/components/mvt/Velocity2DComponent.java
+++ src/main/java/com/nzt/gdx/ashley/components/mvt/Velocity2DComponent.java
@@ -8,29 +8,34 @@ import com.nzt.gdx.ashley.components.physx.B2DBodyComponent;
 
 public class Velocity2DComponent extends PoolableComponent {
 
-    public static ComponentMapper<Velocity2DComponent> mapper = ComponentMapper.getFor(Velocity2DComponent.class);
+	public static ComponentMapper<Velocity2DComponent> mapper = ComponentMapper.getFor(Velocity2DComponent.class);
 
-    public Vector2 velocity = new Vector2();
+	public Vector2 velocity = new Vector2();
 
-    public Velocity2DComponent() {
-        super();
-    }
+	public Velocity2DComponent() {
+		super();
+	}
 
-    @Override
-    public void reset() {
-        this.velocity.setZero();
-    }
+	@Override
+	public void reset() {
+		this.velocity.setZero();
+	}
 
-    public static void updateVelocityFromBody(Entity entity) {
-        Velocity2DComponent velocity2DComponent = mapper.get(entity);
-        if (velocity2DComponent != null) {  //les static n'ont pas de vel
-            B2DBodyComponent b2DBodyComponent = B2DBodyComponent.mapper.get(entity);
-            Vector2 linearVelocity = b2DBodyComponent.body.getLinearVelocity();
-            velocity2DComponent.velocity.set(linearVelocity);
-        }
-    }
+	public static void updateVelocityFromBody(Entity entity) {
+		Velocity2DComponent velocity2DComponent = mapper.get(entity);
+		if (velocity2DComponent != null) { // les static n'ont pas de vel
+			B2DBodyComponent b2DBodyComponent = B2DBodyComponent.mapper.get(entity);
+			Vector2 linearVelocity = b2DBodyComponent.body.getLinearVelocity();
+			velocity2DComponent.velocity.set(linearVelocity);
+		}
+	}
 
-    public float getSpeed() {
-        return velocity.len();
-    }
+	// TODO a voir si  pas crée une variable simple
+	public Vector2 getDir() {
+		return velocity.cpy().nor();
+	}
+
+	public float getSpeed() {
+		return velocity.len();
+	}
 }
