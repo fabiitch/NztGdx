8b2c750498998de6547b9f27a5f094342538bd71
fabiitch <fabien.occelli@hotmail.fr>
2021-02-26 02:10:19 +0100

    useless

--- src/main/java/com/nzt/gdx/math/vectors/VectorUtils.java
+++ src/main/java/com/nzt/gdx/math/vectors/VectorUtils.java
@@ -1,6 +1,7 @@
 package com.nzt.gdx.math.vectors;
 
 import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
 public class VectorUtils {
@@ -15,6 +16,16 @@ public class VectorUtils {
         V scl = directionTo.scl(dst / time);
         return scl;
     }
+
+    //TODO chai pas http://www.3dkingdoms.com/weekly/weekly.php?a=2
+    //https://gamedev.stackexchange.com/questions/23672/determine-resulting-angle-of-wall-collision
+//    public static <V extends Vector<V>> V reflect(Vector2 vector, Vector2 normal, Vector2 result) {
+//        // return vector - 2 * Vector2.Dot(vector, normal) * normal;
+//        Vector2 tmp = new Vector2();
+//        result.set(vector);
+//        tmp.set(vector).dot(normal)
+//        return
+//    }
 
     public static <V extends Vector<V>> V getFarthest(V v1, Array<V> arrayV2) {
         float dstClose = 0;
