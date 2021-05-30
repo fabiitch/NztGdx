895ba13230eafd77ba54f132c13630197b543c4c
fabiitch <fabien.occelli@hotmail.fr>
2021-05-31 00:56:55 +0200

    heap in MB

--- src/main/java/com/nzt/gdx/debug/hud/HudDebugApplicationInfo.java
+++ src/main/java/com/nzt/gdx/debug/hud/HudDebugApplicationInfo.java
@@ -6,43 +6,44 @@ import com.nzt.gdx.debug.hud.base.HudDebug;
 
 public class HudDebugApplicationInfo {
 
-	public HudDebugApplicationInfo(int positionOnStage, Color color) {
-		HudDebug.addItem("FPS", Gdx.graphics.getFramesPerSecond(), positionOnStage, color);
-		HudDebug.addItem("DT", 0, positionOnStage, color);
-		HudDebug.addItem("NativeHeap", Gdx.app.getNativeHeap(), positionOnStage, color);
-		HudDebug.addItem("JavaHeap", Gdx.app.getJavaHeap(), positionOnStage, color);
-		HudDebug.addItem("NbThread", "" + Thread.activeCount(), positionOnStage, color);
-		HudDebug.addItem("CurrentThread", Thread.currentThread().getName(), positionOnStage, color);
-	}
+    public HudDebugApplicationInfo(int positionOnStage, Color color) {
+        HudDebug.addItem("FPS", Gdx.graphics.getFramesPerSecond(), positionOnStage, color);
+        HudDebug.addItem("DT", 0, positionOnStage, color);
+        HudDebug.addItem("JavaHeap", Gdx.app.getJavaHeap() / (1024L * 1024L) + " MB", positionOnStage, color);
+        HudDebug.addItem("NativeHeap", Gdx.app.getNativeHeap() / (1024L * 1024L) + " MB", positionOnStage, color);
+        HudDebug.addItem("NbThread", "" + Thread.activeCount(), positionOnStage, color);
+        HudDebug.addItem("CurrentThread", Thread.currentThread().getName(), positionOnStage, color);
+    }
 
-	public HudDebugApplicationInfo() {
-		this(HudDebugPosition.TOP_LEFT, Color.WHITE);
-	}
+    public HudDebugApplicationInfo() {
+        this(HudDebugPosition.TOP_LEFT, Color.WHITE);
+    }
 
-	public void update(float dt) {
-		HudDebug.update("FPS", Gdx.graphics.getFramesPerSecond());
-		HudDebug.update("DT", dt);
-		HudDebug.update("NativeHeap", Gdx.app.getNativeHeap());
-		HudDebug.update("JavaHeap", Gdx.app.getJavaHeap());
-		HudDebug.update("NbThread", "" + Thread.activeCount());
-		HudDebug.update("CurrentThread", Thread.currentThread().getName());
-	}
+    public void update(float dt) {
+        HudDebug.update("FPS", Gdx.graphics.getFramesPerSecond());
+        HudDebug.update("DT", dt);
+        HudDebug.update("JavaHeap", Gdx.app.getJavaHeap() / (1024L * 1024L) + " MB");
+        HudDebug.update("NativeHeap", Gdx.app.getNativeHeap() / (1024L * 1024L) + " MB");
 
-	public void changeColor(Color color) {
-		HudDebug.changeColor("FPS", color);
-		HudDebug.changeColor("DT", color);
-		HudDebug.changeColor("NativeHeap", color);
-		HudDebug.changeColor("JavaHeap", color);
-		HudDebug.changeColor("NbThread", color);
-		HudDebug.changeColor("CurrentThread", color);
-	}
+        HudDebug.update("NbThread", "" + Thread.activeCount());
+        HudDebug.update("CurrentThread", Thread.currentThread().getName());
+    }
 
-	public void removeFromHudDebug() {
-		HudDebug.remove("FPS");
-		HudDebug.remove("DT");
-		HudDebug.remove("NativeHeap");
-		HudDebug.remove("JavaHeap");
-		HudDebug.remove("NbThread");
-		HudDebug.remove("CurrentThread");
-	}
+    public void changeColor(Color color) {
+        HudDebug.changeColor("FPS", color);
+        HudDebug.changeColor("DT", color);
+        HudDebug.changeColor("NativeHeap", color);
+        HudDebug.changeColor("JavaHeap", color);
+        HudDebug.changeColor("NbThread", color);
+        HudDebug.changeColor("CurrentThread", color);
+    }
+
+    public void removeFromHudDebug() {
+        HudDebug.remove("FPS");
+        HudDebug.remove("DT");
+        HudDebug.remove("NativeHeap");
+        HudDebug.remove("JavaHeap");
+        HudDebug.remove("NbThread");
+        HudDebug.remove("CurrentThread");
+    }
 }
