3a489412f22d26721cccb1b4b54c852fba1a0ba4
fabiitch <fabien.occelli@hotmail.fr>
2021-06-17 23:38:13 +0200

    remove new V2 on circle utils

--- src/main/java/com/nzt/gdx/math/shape/utils/CircleUtils.java
+++ src/main/java/com/nzt/gdx/math/shape/utils/CircleUtils.java
@@ -4,33 +4,24 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
-//TODO tout revoir si le commit circle est merge
 public class CircleUtils {
-	
+
     private CircleUtils() {
     }
-    
-    // TODO remove new Vector2
-    public static Vector2 positionOnACircleWithAngle(float xP, float yP, float rayon, float angleRadian) {
-        Vector2 position = new Vector2();
-        position.x = xP + rayon * MathUtils.cos(angleRadian);
-        position.y = yP + rayon * MathUtils.sin(angleRadian);
-        return position;
+
+    public static Vector2 positionOnACircleWithAngle(float xP, float yP, float rayon, float angleRadian, Vector2 returnV) {
+        returnV.x = xP + rayon * MathUtils.cos(angleRadian);
+        returnV.y = yP + rayon * MathUtils.sin(angleRadian);
+        return returnV;
     }
 
-    // TODO remove new Vector2
-    public static Vector2 positionOnACircleWithAngle(Vector2 positionStart, float rayon, float angleRadian) {
-        Vector2 position = new Vector2();
-        position.x = positionStart.x + rayon * MathUtils.cos(angleRadian);
-        position.y = positionStart.y + rayon * MathUtils.sin(angleRadian);
-        return position;
+    public static Vector2 positionOnACircleWithAngle(Vector2 positionStart, float rayon, float angleRadian, Vector2 returnV) {
+        return positionOnACircleWithAngle(positionStart.x, positionStart.y, rayon, angleRadian, returnV);
     }
 
-    // TODO remove new Vector3
-    public static Vector3 positionOnACircleWithAngle(Vector3 positionStart, float rayon, float angleRadian) {
-        Vector3 position = new Vector3();
-        position.x = positionStart.x + rayon * MathUtils.cos(angleRadian);
-        position.y = positionStart.y + rayon * MathUtils.sin(angleRadian);
-        return position;
+    public static Vector3 positionOnACircleWithAngle(Vector3 positionStart, float rayon, float angleRadian, Vector3 returnV) {
+        returnV.x = positionStart.x + rayon * MathUtils.cos(angleRadian);
+        returnV.y = positionStart.y + rayon * MathUtils.sin(angleRadian);
+        return returnV;
     }
 }
